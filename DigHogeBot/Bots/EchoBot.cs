e4738da334e0cfcef8a65d77fc967d34df5f250b
Manabu Tonosaki <manabu@tomarika.com>
2023-04-21 07:15:28 +0900

    first deployment

---
+++ DigHogeBot/Bots/EchoBot.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DigHogeBot.Bots {
+    public class EchoBot: ActivityHandler {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken) {
+            var replyText = $"今、{turnContext.Activity.Text} って言いました？";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken) {
+            var welcomeText = "こんにちは。DIG Hogeボットです！";
+            foreach (var member in membersAdded) {
+                if (member.Id != turnContext.Activity.Recipient.Id) {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
